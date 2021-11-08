4c094f3e54d4405c766b456024535eb29aa0b89f
jeong-yeji <jeongyeji.mail@gmail.com>
2021-11-08 16:58:35 +0900

    폴더 변경 3.3

---
+++ chap03/3.3/var.js
@@ -0,0 +1,14 @@
+// 변수 이용
+const odd = '홀수입니다';
+const even = '짝수입니다';
+module.exports = {
+    odd,
+    even,
+};
+
+// 3.4 exports 모듈 이용
+// exports.odd = '홀수입니다.';
+// exports.even = '짝수입니다';
+
+// module.exports와 exports는 같은 객체 참조
+// console.log(module.exports == exports) // true
