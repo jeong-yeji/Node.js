df2d12ad2237cdcb33af470962a9acbb1f8b1e8e
jeong-yeji <jeongyeji.mail@gmail.com>
2021-12-07 06:14:23 +0900

    6.3 Router 객체를 통한 라우팅 분리

---
+++ chap06/6.3/learn-express/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+const indexRouter = require('./routes'); // index.js는 생략 가능
+const userRouter = require('./routes/user');
+const router = require('./routes');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+        name: 'session-cookie',
+    })
+);
+
+//Router 객체로 라우팅 깔끔하게 관리
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+// 주소는 같지만 메소드 다른 코드 하나로 처리 가능
+router
+    .route('/abc')
+    .get((req, res) => {
+        res.send('GET /abc');
+    })
+    .post((req, res) => {
+        res.send('POST /abc');
+    });
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
---
+++ chap06/6.3/learn-express/routes/user.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET /user 라우터
+router.get('/', (req, res) => {
+    res.send('Hello, User');
+});
+
+// 1. 주소는 같지만 메소드 다른 코드 하나로 처리 가능
+router
+    .route('/abc')
+    .get((req, res) => {
+        res.send('GET /abc');
+    })
+    .post((req, res) => {
+        res.send('POST /abc');
+    });
+
+// 2.라우터 주소에는 정규표현식과 같은 특수 패턴 사용 가능
+// 라우터 매개변수 : /user/1
+// 주의! 일반 라우터보다 뒤에 위치해야됨!
+// :id면 req.params.id로 조회 가능
+// router.get('/user/:id', function (req, res) {
+//     console.log(req.params, req.query);
+// });
+
+module.exports = router;
